79f5fec279a486ed6bdacbb4e5661df3a40fb3a0
chao.chen <chao.chen@kingstarfintech.com>
2023-06-11 14:12:08 +0800

    update

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
-import (
-	"log"
-	"os"
-)
-
 func main() {
-	err := os.Setenv("FYNE_FONT", "msyhl.ttc")
-	if err != nil {
-		log.Fatalf("%v\n", err)
-		return
-	}
+	//err := os.Setenv("FYNE_FONT", "msyhl.ttc")
+	//if err != nil {
+	//	log.Fatalf("%v\n", err)
+	//	return
+	//}
 	EtcdView()
 }
